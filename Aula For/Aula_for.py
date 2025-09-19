a92bcd142817255792b65cb6a57cdf3f46b019c8
Rodrigo Borges <digoborges999@gmail.com>
2025-09-18 21:55:59 -0300

    Estudo

---
+++ Aula For/Aula_for.py
@@ -0,0 +1,40 @@
+for c in range (1,11):
+   print ("oi")
+print("fim")
+
+print(50*"-")
+
+for c in range (0,10):
+    print (c)
+print("fim")
+
+print(50*"-")
+
+for c in range (10,0,-1):
+    print (c)
+print("fim")
+
+print(50*"-")
+
+n = int( input("Digite um numero:"))
+for c in range (0,n):
+    print (c)
+print("fim")
+
+print(50*"-")
+
+i=int(input("Digite o inicio:"))
+f=int(input("Digite o fim:"))
+p=int(input("Digite o passo:"))
+
+for c in range (i,f+1,p):
+    print (c)
+print("Fim")
+
+print(50*"-")
+
+soma=0
+for c in range(1,4):
+    N=int(input("Digite o numero:"))
+    soma+=N
+print(f"A somatória foi {soma}")
